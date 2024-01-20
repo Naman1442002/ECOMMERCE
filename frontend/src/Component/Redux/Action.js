6ddde13d1a9d9737adcc689a14cbcb59633a57c0
Naman <115204247+Naman1442002@users.noreply.github.com>
2024-01-20 17:03:51 +0530

    Update Action.js

--- frontend/src/Component/Redux/Action.js
+++ frontend/src/Component/Redux/Action.js
@@ -34,7 +34,7 @@ export const fetchItems=()=>
 {
     return (dispatch)=>{
         dispatch(fetch_items_request());
-        axios.get("https://shivpalcommerce.onrender.com/products")
+        axios.get("https://dummyjson.com/products")
         .then((resp)=>{
             dispatch(fetch_items_success(resp.data))})
         .catch((err)=>{dispatch(fetch_items_failure(err.message))})
